891292d98d041e6b88c9452476c375cdc4d46474
Aaron Cleveland <toscleveland@gmail.com>
2020-02-22 20:41:12 -0600

    Init

---
+++ Dashboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dashboard
+//
+//  Created by Aaron Cleveland on 2/22/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dashboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dashboard
+//
+//  Created by Aaron Cleveland on 2/22/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
